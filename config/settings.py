a6d1d78a4e7e6e6f4ea6dcaf387ada122bde2f9f
Dominic Zacheaus <DOMINICZACHEZ@GMAIL.COM>
2026-09-06 19:24:01 +0300

    Harden production static and domain settings

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'website' / 'static',
@@ -131,4 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+
+
+CSRF_TRUSTED_ORIGINS = ['https://chefdominiczacheaus.com', 'https://www.chefdominiczacheaus.com', 'https://dominics-kitchen.onrender.com']
 
